efd87ef0438035d0321c4fdaf45906042ca87409
Alex V <alekceyanatolievich@gmail.com>
2021-07-20 00:27:44 +0300

    fixed bool , add import math, del return

--- alexv_hw1.py
+++ alexv_hw1.py
@@ -1,16 +1,6 @@
-
-
-def print_type(variable):
-    """This function prints the type of supplied variable
-
-    Args:
-        variable (Any): variable to print type for
-    """
-    # fill the gaps ...
-    x = type(variable)
-    return print(x)
-
-
+import math
+def print_type(var):
+    print(type(var))
 def do_action(variable):
     """Does the action depending on variable's type. (hint: you can either use print function right here, 
     or return the result. For now it doesn't matter)
@@ -19,30 +9,24 @@ def do_action(variable):
         float: add π(pi) (from math.pi, don't forget the import~!) to it and print the result
         bool: inverse it (e.g if you have True, make it False) and print the result
         list: print elements in reversed order
-
     Args:
         variable (Any): variable to perform action on
     """
-    if isinstance(variable, int):
-        variable = variable * variable
-        return print(variable)
+    if isinstance(variable, bool):
+         print(not variable)
+    elif isinstance(variable, int):
+         print(variable * variable)
     elif isinstance(variable, float):
-        return print(variable + math.pi)
-    elif isinstance(variable, bool):
-        return print(not variable)
+         print(variable + math.pi)
     elif isinstance(variable, list):
         arr = variable[::-1]
-        return print(arr)
+        print(arr)
     else:
-        return print("Action is not defined")
+         print("Action is not defined")
 
 
 variables = [42, 45.0, True, False, [16, 9, 43, 65, 97, 0]]
 
-# Please, for all elements in `variables` list print the following:
-#  the type of variable using `print_type` function
-#  and the action for variable using `do_action` function
-
-for i in variables:
-    print_type(i)
-    do_action(i)
+for item in variables:
+    print_type(item)
+    do_action(item)
